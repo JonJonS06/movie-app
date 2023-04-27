043e2cd55063960f10d1746bd5917432a96a6748
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-04-26 22:43:06 -0400

    added API keys for OMDb, YouTube, and GoogleMaps to javascript

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,3 @@
 let requestMovieUrl = ' http://www.omdbapi.com/?i=tt3896198&apikey=9b9137db';
 let requestVideoUrl = 'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCHGRBU3FkHUBex7-Ry8oZIxA-fQvhVnZc&part=snippet,contentDetails,statistics,status';
+let requestMapUrl = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places'
