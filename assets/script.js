a2b4505c713b2a851241e7946d208e95b5ecd245
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-04-27 21:44:07 -0400

    working on eventlistener for moreinfo btn

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,20 @@
-let requestMovieUrl = ' http://www.omdbapi.com/?i=tt3896198&apikey=9b9137db';
+let requestMovieUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=9b9137db';
 let requestVideoUrl = 'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCHGRBU3FkHUBex7-Ry8oZIxA-fQvhVnZc&part=snippet,contentDetails,statistics,status';
 let requestMapUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCHGRBU3FkHUBex7-Ry8oZIxA-fQvhVnZc&libraries=places'
 let learnMoreBtn = document.querySelector('learnMore-btn');
 
 
-
-learnMoreBtn.addEventListener('click', function() {
-    window.location.replace("");
-})
+    
 
 
+ 
+    
+let MoreInfo = function (event) {
+    let getMoreInfo = 'http://www.omdbapi.com/?s=&apikey=9b9137db' 
+ 
+    window.location.replace("./learnmore.html");
+}  
+    
+
+
+learnMoreBtn.addEventListener('click', )
