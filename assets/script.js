38ab90089ea3b4166d843fded87e47993953cbdd
[Ashley Braxton] <[ashley.braxton25@gmail.com]>
2023-05-01 18:04:10 -0400

    chenged card generation to Jquery and removed the tester calling of the function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 let requestMovieUrl = ' http://www.omdbapi.com/?apikey=9b9137db';
 let requestVideoUrl = 'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCHGRBU3FkHUBex7-Ry8oZIxA-fQvhVnZc&part=snippet,contentDetails,statistics,status';
 let requestMapUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCHGRBU3FkHUBex7-Ry8oZIxA-fQvhVnZc&libraries=places'
-
+var resultContainer = document.getElementById('results')
 var getMovieQuery = function(movie) {
     var apiUrl = requestMovieUrl + '&s=' + movie;
 
@@ -10,8 +10,8 @@ var getMovieQuery = function(movie) {
             if (response.ok) {
                 console.log(response);
                 response.json().then(function (data) {
-                    console.log(data);
-                    // displayResults(data);
+                    console.log(data.Search);
+                    displayMovieCards(data)
                 });
             } else {
                 // TODO add modal after adding html and css styling
@@ -24,8 +24,6 @@ var getMovieQuery = function(movie) {
             // modal.textContent = 'Unable to connect to OMDb database';
         })
 };
-getMovieQuery('batman');
-console.log('hi');
  
 var searchBtn = document.querySelector(".ui button");
 var input = document.querySelector("input")
@@ -43,41 +41,62 @@ var searchMovie = function(event) {
 };
 searchBtn.addEventListener("click", searchMovie);
 
-// below is the code to dynamically generate the movie cards
-  const container = document.getElementById('results');
-  
-  apiResult.forEach((result) => {
+function displayMovieCards (data) {
+  var movieArray= data.Search;
+  for (var i = 0; i < movieArray.length; i++) {
+    var data = movieArray[i];
+    var uiCard = document.createElement('div')
+    $(uiCard).addClass("ui card")
+    var img = $('<img />', { 
+      src: data.Poster,
+    });
+    img.appendTo($(uiCard));
+    var main = document.createElement('div');
+    $(main).addClass("content");
+    var title = document.createElement('a');
+    $(title).addClass("header");
+    title.textContent = data.Title;
+    main.append(title);
+    var meta = document.createElement('div');
+    $(meta).addClass("meta");
+    var year = document.createElement('span');
+    $(year).addClass("year");
+    year.textContent= data.Year;
+    meta.append(year);
+    var type = document.createElement('span');
+    $(type).addClass("type");
+    type.textContent= data.Type;
+    meta.append(type);
+    main.append(meta);
+    var description = document.createElement('div');
+    $(description).addClass("description");
+    description.textContent= "Placeholder";
+    main.append(description);
+    uiCard.append(main);
+    var extra = document.createElement('div');
+    $(extra).addClass("extra content");
+    var learnMore = document.createElement('a');
+    var moreButton = document.createElement('button');
+    $(moreButton).addClass("ui button");
+    moreButton.textContent="Learn More";
+    learnMore.append(moreButton);
+    extra.append(learnMore);
+    var favorite = document.createElement('div');
+    $(favorite).addClass("ui animated button");
+    var visible = document.createElement('div');
+    $(visible).addClass("visible content");
+    visible.textContent= "Favorite";
+    favorite.append(visible);
+    var hidden = document.createElement('div');
+    $(hidden).addClass("hidden content");
+    var icon = document.createElement('i');
+    $(icon).addClass("heart icon");
+    hidden.append(icon);
+    favorite.append(hidden);
+    extra.append(favorite);
+    uiCard.append(extra);
+    resultContainer.append(uiCard);
 
-    const card = document.createElement('div');
-    card.classList = 'card-body';
-  
-    const content = `
-    <div class="ui card">
-    <div class="image">
-      <img src="./batman-superman.jpg">
-    </div>
-    <div class="content">
-      <a class="header">${result.title}</a>
-      <div class="meta">
-        <span class="date">${result.releaseYear}</span>
-        <span class="runtime">${result.runTime}</span>
-      </div>
-      <div class="description">
-        ${result.description}
-      </div>
-    </div>
-    <div class="extra content">
-      <a>
-        <button class="ui button">Learn More</button>
-      </a>
-      <div class="ui animated button" tabindex="0">
-        <div class="visible content">Favorite</div>
-        <div class="hidden content">
-          <i class="heart icon"></i>
-        </div>
-    </div>
-  </div>
-    `;
-  
-    container.innerHTML += content;
-  })
+
+  }
+}
