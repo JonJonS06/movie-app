a2d78bf9b2e13a773d332894aea19e4f8846d18f
Houston Culpepper <howacul@gmail.com>
2023-05-03 19:18:54 -0400

    added odmb api request used for the idmbID

---
+++ assets/learnMore.js
@@ -0,0 +1,20 @@
+let requestMovieUrl = ' https://www.omdbapi.com/?apikey=9b9137db';
+
+var getMoreInfo = function(imdbID) {
+    var apiUrl = requestMovieUrl + '&i=' + imdbID;
+
+    fetch(apiUrl)
+        .then(function (response) {  
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayMore(data)
+                });
+            } else {
+                modalDescription.textContent = 'Error: ' + response.statusText;
+            }
+        })
+        .catch(function (error) {
+            modalDescription.textContent = 'Unable to connect to OMDb database';
+            $('.ui.modal').modal('show');
+        })
+};
